2920fc880332cd09491ee7b286d549ad497f12d0
Yessankyila Marius SILUE <ysilue2002@gmail.com>
2025-06-17 21:39:18 +0000

    Ajout complet des fichiers

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css"; // ✅ Import du fichier CSS avec les directives Tailwind
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
